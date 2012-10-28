1405446eb9014783e9b065bae995cb32fa944604
RaymanFX <raymanfx@gmail.com>
2012-10-28 18:19:17 +0100

    Revert "Implement the FileChooser. Does not do aynthing at the moment, I am working on it."
    This reverts commit 5b602cd9774b83f7b71c596390ed83c671535a73.

--- src/com/raymanfx/settings/extras/Gapps.java
+++ src/com/raymanfx/settings/extras/Gapps.java
@@ -21,7 +21,6 @@ import android.content.Intent;
 import android.net.Uri;
 import java.util.Formatter;
 import com.raymanfx.settings.tools.RootChecker;
-import com.raymanfx.settings.filepicker.FileChooser;
 
 import android.os.Bundle;
 import android.preference.ListPreference;
@@ -61,7 +60,6 @@ public class Gapps extends SettingsPreferenceFragment {
 	    RootChecker.runRootCommand("reboot recovery");
         } else if (preference == mFilePicker) {
             /* WIP */
-	    startActivity(new Intent(this, FileChooser.class));
         }
 
         return super.onPreferenceTreeClick(preferenceScreen, preference);
